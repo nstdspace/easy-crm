bd35fc19eaa95112073efc2f9fac38205f9cc4f6
Maksim Rotmann <maksim.rotmann@l21s.de>
2025-06-17 00:14:50 +0200

    Fix cors settings

--- easy-crm-backend/src/main/kotlin/de/nstdspace/easycrmbackend/EasyCrmBackendApplication.kt
+++ easy-crm-backend/src/main/kotlin/de/nstdspace/easycrmbackend/EasyCrmBackendApplication.kt
@@ -1,9 +1,14 @@
 package de.nstdspace.easycrmbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication class EasyCrmBackendApplication
+@SpringBootApplication
+@EnableConfigurationProperties(
+    CorsConfig::class
+)
+class EasyCrmBackendApplication
 
 fun main(args: Array<String>) {
     runApplication<EasyCrmBackendApplication>(*args)
--- easy-crm-backend/src/main/kotlin/de/nstdspace/easycrmbackend/WebSecurityConfig.kt
+++ easy-crm-backend/src/main/kotlin/de/nstdspace/easycrmbackend/WebSecurityConfig.kt
@@ -1,6 +1,6 @@
 package de.nstdspace.easycrmbackend
 
-import java.util.*
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -10,14 +10,21 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
+
+
+@ConfigurationProperties("cors")
+data class CorsConfig(
+    val allowedOrigins: List<String>
+)
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig {
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
+    fun corsConfigurationSource(config: CorsConfig): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:5173")
+        configuration.allowedOrigins = config.allowedOrigins
         configuration.allowedMethods = listOf("GET", "POST")
         configuration.allowedHeaders = listOf("Authorization")
         val source = UrlBasedCorsConfigurationSource()
